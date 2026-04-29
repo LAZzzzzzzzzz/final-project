151aaddc4c23551011a39f4bcf5565d360e0929d
umph <lukalazuashvili123@gmail.com>
2026-04-30 00:54:58 +0400

    create react app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
+function App() {
+
+  return <QueryClientProvider client={queryClient}>
+      <div>todo</div>
+    </QueryClientProvider>
+}
+
+export default App
